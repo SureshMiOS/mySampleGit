acc2c91ab1c16645a860b5b49dab470e08480777
SureshMiOS <suresh@DreamMacServer.local>
2017-04-21 11:20:26 +0530

    My first commit

---
+++ MyTestGit/MyTestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestGit
+//
+//  Created by Suresh Murugaiyan on 4/21/17.
+//  Copyright © 2017 dreamorbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestGit/MyTestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestGit
+//
+//  Created by Suresh Murugaiyan on 4/21/17.
+//  Copyright © 2017 dreamorbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
